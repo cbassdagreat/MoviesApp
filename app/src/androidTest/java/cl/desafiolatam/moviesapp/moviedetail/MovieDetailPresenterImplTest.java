37664d186274812d7c33f6130a5ef03712873e50
Cbass <claveria.sebastian@gmail.com>
2022-03-17 18:59:40 -0300

    Initial commit

---
+++ app/src/androidTest/java/cl/desafiolatam/moviesapp/moviedetail/MovieDetailPresenterImplTest.java
@@ -0,0 +1,15 @@
+package cl.desafiolatam.moviesapp.moviedetail;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MovieDetailPresenterImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
